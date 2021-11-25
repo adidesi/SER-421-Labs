912a8b1bfeb180a7010340288205b9aa7d637f54
Aditya Desai <adidesi95@gmail.com>
2021-11-24 22:44:46 -0700

    Lab 5 init

---
+++ LAB 5/Solution/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
